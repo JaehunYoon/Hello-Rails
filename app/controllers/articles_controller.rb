dea113bc57fe4afffde3e384380e9495ceca0ccf
JaehunYoon <goodasd123@gmail.com>
2019-01-02 19:45:54 +0900

    Rails : CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,46 @@
+# Recommended CRUD Action Index
+# Index -> show -> new -> edit -> create -> update -> destroy
+
 class ArticlesController < ApplicationController
-    def new
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+    # render plain: params[:article].inspect
+  end
 
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
+  def update
+    @article = Article.find(params[:id])
 
-        @article.save
-        redirect_to @article
-        # render plain: params[:article].inspect
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
     end
 end
