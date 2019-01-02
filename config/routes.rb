b9a5f13da9c1cc40f016c7991069441130fdb11a
JaehunYoon <goodasd123@gmail.com>
2019-01-02 10:04:21 +0900

    Rails : Form Helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :articles
+
   root 'welcome#index'
   # Rails 애플리케이션의 기본 URL에 대한 접속 요청을 welcome 컨트롤러의 index 액션으로 할당
 end
