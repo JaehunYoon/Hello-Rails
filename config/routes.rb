c5f1a9016c55e0d1181a93cc9ee25bc5a6659d05
JaehunYoon <goodasd123@gmail.com>
2019-01-02 11:25:58 +0900

    Rails : Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resource :articles
+  resources :articles
 
   root 'welcome#index'
   # Rails 애플리케이션의 기본 URL에 대한 접속 요청을 welcome 컨트롤러의 index 액션으로 할당
