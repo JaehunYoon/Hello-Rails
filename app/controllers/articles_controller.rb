b239dd5c2c8d9414b4bf15b7cb48ace7467bad2a
JaehunYoon <goodasd123@gmail.com>
2019-01-04 10:09:12 +0900

    Rails : Certification using http_basic_authenticate_with method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@
 # Index -> show -> new -> edit -> create -> update -> destroy
 
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
